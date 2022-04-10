520e7b1dd86034c1daab66b384ad92d8887eaa2b
Sfertix <65566305+Sfertix@users.noreply.github.com>
2022-04-10 23:36:45 +0200

    Ajout des redirections vers la page de connexion

--- inscription.php
+++ inscription.php
@@ -1,113 +1,57 @@
-<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="author" content="NoS1gnal"/>
+<?php 
+    require_once 'config.php'; // On inclu la connexion à la bdd
 
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-            <title>Inscription</title>
-        </head>
-        <body>
-        <div class="login-form">
-            <?php 
-                if(isset($_GET['reg_err']))
-                {
-                    $err = htmlspecialchars($_GET['reg_err']);
+    // Si les variables existent et qu'elles ne sont pas vides
+    if(!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_retype']))
+    {
+        // Patch XSS
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        $password_retype = htmlspecialchars($_POST['password_retype']);
 
-                    switch($err)
-                    {
-                        case 'success':
-                        ?>
-                            <div class="alert alert-success">
-                                <strong>Succès</strong> inscription réussie !
-                            </div>
-                        <?php
-                        break;
+        // On vérifie si l'utilisateur existe
+        $check = $bdd->prepare('SELECT pseudo, email, password FROM utilisateurs WHERE email = ?');
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();
 
-                        case 'password':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> mot de passe différent
-                            </div>
-                        <?php
-                        break;
+        $email = strtolower($email); // on transforme toute les lettres majuscule en minuscule pour éviter que Foo@gmail.com et foo@gmail.com soient deux compte différents ..
+        
+        // Si la requete renvoie un 0 alors l'utilisateur n'existe pas 
+        if($row == 0){ 
+            if(strlen($pseudo) <= 100){ // On verifie que la longueur du pseudo <= 100
+                if(strlen($email) <= 100){ // On verifie que la longueur du mail <= 100
+                    if(filter_var($email, FILTER_VALIDATE_EMAIL)){ // Si l'email est de la bonne forme
+                        if($password === $password_retype){ // si les deux mdp saisis sont bon
 
-                        case 'email':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email non valide
-                            </div>
-                        <?php
-                        break;
-
-                        case 'email_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> email trop long
-                            </div>
-                        <?php 
-                        break;
-
-                        case 'pseudo_length':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> pseudo trop long
-                            </div>
-                        <?php 
-                        case 'already':
-                        ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur</strong> compte deja existant
-                            </div>
-                        <?php 
-
-                    }
-                }
-                ?>
-            
-            <form action="inscription_traitement.php" method="post">
-                <h2 class="text-center">Inscription</h2>       
-                <div class="form-group">
-                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block">Inscription</button>
-                </div>   
-            </form>
-        </div>
-        <style>
-            .login-form {
-                width: 340px;
-                margin: 50px auto;
-            }
-            .login-form form {
-                margin-bottom: 15px;
-                background: #f7f7f7;
-                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-                padding: 30px;
-            }
-            .login-form h2 {
-                margin: 0 0 15px;
-            }
-            .form-control, .btn {
-                min-height: 38px;
-                border-radius: 2px;
-            }
-            .btn {        
-                font-size: 15px;
-                font-weight: bold;
-            }
-        </style>
-        </body>
-</html>
+                            // On hash le mot de passe avec Bcrypt, via un coût de 12
+                            $cost = ['cost' => 12];
+                            $password = password_hash($password, PASSWORD_BCRYPT, $cost);
+                            
+                            // On stock l'adresse IP
+                            $ip = $_SERVER['REMOTE_ADDR']; 
+                             /*
+                              ATTENTION
+                              Verifiez bien que le champs token est présent dans votre table utilisateurs, il a été rajouté APRÈS la vidéo
+                              le .sql est dispo pensez à l'importer ! 
+                              ATTENTION
+                            */
+                            // On insère dans la base de données
+                            $insert = $bdd->prepare('INSERT INTO utilisateurs(pseudo, email, password, ip, token) VALUES(:pseudo, :email, :password, :ip, :token)');
+                            $insert->execute(array(
+                                'pseudo' => $pseudo,
+                                'email' => $email,
+                                'password' => $password,
+                                'ip' => $ip,
+                                'token' => bin2hex(openssl_random_pseudo_bytes(64))
+                            ));
+                            // On redirige avec le message de succès
+                            header('Location:index.php?login_err=success');
+                            die();
+                        }else{ header('Location: inscription.php?reg_err=password'); die();}
+                    }else{ header('Location: inscription.php?reg_err=email'); die();}
+                }else{ header('Location: inscription.php?reg_err=email_length'); die();}
+            }else{ header('Location: inscription.php?reg_err=pseudo_length'); die();}
+        }else{ header('Location: index.php?login_err=already'); die();}
+    }
